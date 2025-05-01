23e85264d4f9530c0cade876e81353a9910366c1
KaspOu <kaspbouh+gitkraken@gmail.com>
2025-05-01 21:40:21 +0200

    ✨ Buffs / Debuffs positions and directions

--- Core/localization.en.lua
+++ Core/localization.en.lua
@@ -13,6 +13,7 @@ l.DESC          = "Changes party/raid Buffs & Debuffs|r\n\n"
 .." - Resize buffs/debuffs\n\n"
 .." - Increase max buffs/debuffs displayed\n\n"
 .." - Multiline display\n\n"
+.." - Relative X/Y position\n\n"
 .." - Customizable orientation\n\n"
 l.OPTIONS_TITLE = format("%s - Options", l.VERS_TITLE);
 
@@ -21,31 +22,46 @@ l.MSG_LOADED         = format("%s loaded and active", l.VERS_TITLE);
 l.INIT_FAILED = format("%s not initialized correctly (conflict?)!", l.VERS_TITLE);
 
 local required = l.YL.."*";
-l.OPTION_BUFFS_HEADER = "Buffs / Debuffs";
+l.OPTION_BUFFS_HEADER = "Buffs / Debuffs"
+l.OPTION_ORIENTATION_LeftThenUp = "Left, then Up"
+l.OPTION_ORIENTATION_LeftThenUp_Default = l.DEFAULT.."Left, then Up (default)"
+l.OPTION_ORIENTATION_UpThenLeft = "Up, then Left"
+l.OPTION_ORIENTATION_RightThenUp = "Right, then Up"
+l.OPTION_ORIENTATION_RightThenUp_Default = l.DEFAULT.."Right, then Up (default)"
+l.OPTION_ORIENTATION_UpThenRight = "Up, then Right"
 l.OPTION_BUFFSSCALE = "Buffs relative size"..required;
 l.OPTION_BUFFSSCALE_TOOLTIP = l.CY.."Wow default: 1"
 l.OPTION_MAXBUFFS = "Max buffs"..required;
 l.OPTION_MAXBUFFS_TOOLTIP = "Max buffs to display\n"..l.CY.."Wow default: "..ns.DEFAULT_MAXBUFFS
 l.OPTION_MAXBUFFS_FORMAT = "%d |4buff:buffs";
-l.OPTION_BUFFSPERLINE = "Buffs per line";
-l.OPTION_BUFFSPERLINE_TOOLTIP = "Number of buff icons per line\n"..l.CY.."Wow default: max"
+l.OPTION_BUFFSPERLINE = "Buffs per line"..required;
+l.OPTION_BUFFSPERLINE_TOOLTIP = "Number of buffs per line\n"..l.CY.."Ignored if greater than max buffs";
 l.OPTION_BUFFSPERLINE_FORMAT = "%d per line"..required;
-l.OPTION_BUFFSVERTICAL = "Buffs aligned vertically"..required;
-l.OPTION_BUFFSVERTICAL_TOOLTIP = "Buffs will be vertically aligned,\nin columns\n"..l.CY.."May be disabled few seconds after a boss kill"
+l.OPTION_BUFFSORIENTATION = "Buffs orientation"..required;
+l.OPTION_BUFFSORIENTATION_TOOLTIP = "Choose how buffs are arranged (/w multiline support)\n"..l.CY.."Default: Left to Right, then Up"
+l.OPTION_BUFFS_RELATIVE_X = "Horizontal offset"..required;
+l.OPTION_BUFFS_RELATIVE_X_TOOLTIP = "Adjust the horizontal position of the buffs";
+l.OPTION_BUFFS_RELATIVE_Y = "Vertical offset"..required;
+l.OPTION_BUFFS_RELATIVE_Y_TOOLTIP = "Adjust the vertical position of the buffs";
 l.OPTION_DEBUFFSSCALE = "Debuffs relative size"..required;
 l.OPTION_DEBUFFSSCALE_TOOLTIP = l.CY.."Wow default: 1"
 l.OPTION_MAXDEBUFFS = "Max debuffs"..required;
 l.OPTION_MAXDEBUFFS_TOOLTIP = "Max debuffs to display\n"..l.CY.."Wow default: "..ns.DEFAULT_MAXBUFFS
 l.OPTION_MAXDEBUFFS_FORMAT = "%d |4debuff:debuffs";
 l.OPTION_DEBUFFSPERLINE = "Debuffs per line"..required;
-l.OPTION_DEBUFFSPERLINE_TOOLTIP = "Number of debuff icons per line\n"..l.CY.."Wow default: max"
+l.OPTION_DEBUFFSPERLINE_TOOLTIP = "Number of debuff icons per line\n"..l.CY.."Ignored if greater than max debuffs";
 l.OPTION_DEBUFFSPERLINE_FORMAT = "%d per line";
-l.OPTION_DEBUFFSVERTICAL = "Debuffs aligned vertically"..required;
-l.OPTION_DEBUFFSVERTICAL_TOOLTIP = "Debuffs will be vertically aligned,\nin columns\n"..l.CY.."May be disabled few seconds after a boss kill"
+l.OPTION_DEBUFFSORIENTATION = "Debuffs orientation"..required;
+l.OPTION_DEBUFFSORIENTATION_TOOLTIP = "Choose how debuffs are arranged ((/w multiline support)\n"..l.CY.."Default: Right to Left, then Up"
+l.OPTION_DEBUFFS_RELATIVE_X = "Horizontal offset"..required;
+l.OPTION_DEBUFFS_RELATIVE_X_TOOLTIP = "Adjust the horizontal position of the debuffs";
+l.OPTION_DEBUFFS_RELATIVE_Y = "Vertical offset"..required;
+l.OPTION_DEBUFFS_RELATIVE_Y_TOOLTIP = "Adjust the vertical position of the debuffs";
 l.OPTION_USETAINTMETHOD = l.CY.."Legacy display for Max buffs/debuffs"..required.." "..l.ALERT
 l.OPTION_USETAINTMETHOD_TOOLTIP = "Unchecked, uses an experimental display\nChecked, uses a stable display, but with one "..l.RDL.."error per session|r, not a big deal..."
 l.OPTION_BUFFS_TAINTWARNING = l.ALERT.." Changing Max buffs/debuffs causes one "..l.RDL.."error per session|r, not a big deal..."
 l.OPTION_BUFFS_FLICKERWARNING = l.INFO.." Repositioning may be affected for a few seconds after a boss kill"
+l.OPTION_BUFFS_RESET = "Cancel any repositioning"
 
 l.OPTION_RESET_OPTIONS = "Reset options";
 l.OPTION_RELOAD_REQUIRED = "Some changes require reloading (write: "..l.YL.."/reload|r )";
--- Core/localization.fr.lua
+++ Core/localization.fr.lua
@@ -1,7 +1,7 @@
 -------------------------------------------------------------------------------
 -- French localization
 -------------------------------------------------------------------------------
-if (GetLocale() == "frFR") then    
+if (GetLocale() == "frFR") then
 local _, ns = ...
 local l = ns.I18N;
 
@@ -14,6 +14,7 @@ l.DESC          = "Change les Buffs & D\195\169buffs de groupe / raid|r\n\n"
 .." - Redimensionne les buffs/d\195\169buffs \n\n"
 .." - Augmente le nombre max de buffs/d\195\169buffs affich\195\169s\n\n"
 .." - Affichage multilignes\n\n"
+.." - Position relative X/Y\n\n"
 .." - Orientation au choix\n\n"
 l.OPTIONS_TITLE = format("%s - Options", l.VERS_TITLE);
 
@@ -22,31 +23,46 @@ l.MSG_LOADED         = format("%s lanc\195\169 et actif", l.VERS_TITLE);
 l.INIT_FAILED = format("%s pas charg\195\169 correctement (conflit ?) !", l.VERS_TITLE);
 
 local required = l.YL.."*";
-l.OPTION_BUFFS_HEADER = "Buffs / Debuffs";
+l.OPTION_BUFFS_HEADER = "Buffs / Debuffs"
+l.OPTION_ORIENTATION_LeftThenUp = "\195\128 Gauche, puis en Haut"
+l.OPTION_ORIENTATION_LeftThenUp_Default = l.DEFAULT.."\195\128 Gauche, puis en Haut (par d\195\169faut)"
+l.OPTION_ORIENTATION_UpThenLeft = "En Haut, puis \195\160 Gauche"
+l.OPTION_ORIENTATION_RightThenUp = "\195\128 Droite, puis en Haut"
+l.OPTION_ORIENTATION_RightThenUp_Default = l.DEFAULT.."\195\128 Droite, puis en Haut (par d\195\169faut)"
+l.OPTION_ORIENTATION_UpThenRight = "En Haut, puis \195\160 Droite"
 l.OPTION_BUFFSSCALE = "Taille des buffs "..required;
 l.OPTION_BUFFSSCALE_TOOLTIP = l.CY.."Par d\195\169faut dans Wow : 1"
 l.OPTION_MAXBUFFS = "Limite de buffs"..required;
 l.OPTION_MAXBUFFS_TOOLTIP = "Nombre maximum de buffs \195\160 afficher\n"..l.CY.."Par d\195\169faut dans Wow : "..ns.DEFAULT_MAXBUFFS;
 l.OPTION_MAXBUFFS_FORMAT = "%d |4buff:buffs";
 l.OPTION_BUFFSPERLINE = "Buffs par ligne"..required;
-l.OPTION_BUFFSPERLINE_TOOLTIP = "Nombre d'ic\195\180nes de buff par ligne\n"..l.CY.."Par d\195\169faut dans Wow : max";
+l.OPTION_BUFFSPERLINE_TOOLTIP = "Nombre de buffs par ligne\n"..l.CY.."Ignor\195\169 si sup\195\169rieur \195\160 la Limite";
 l.OPTION_BUFFSPERLINE_FORMAT = "%d par ligne";
-l.OPTION_BUFFSVERTICAL = "Aligner les buffs verticalement"..required;
-l.OPTION_BUFFSVERTICAL_TOOLTIP = "Les buffs seront align\195\169s verticalement, en colonnes\n"..l.CY.."D\195\169sactiv\195\169 \195\160 la mort d'un boss";
+l.OPTION_BUFFSORIENTATION = "Orientation des buffs"..required;
+l.OPTION_BUFFSORIENTATION_TOOLTIP = "Choisissez l\'arrangement des buffs (supporte le multiligne)\n"..l.CY.."Par d\195\169faut : "..l.OPTION_ORIENTATION_LeftThenUp
+l.OPTION_BUFFS_RELATIVE_X = "D\195\169calage horizontal"..required;
+l.OPTION_BUFFS_RELATIVE_X_TOOLTIP = "Ajustez horizontalement la position initiale des buffs";
+l.OPTION_BUFFS_RELATIVE_Y = "D\195\169calage vertical"..required;
+l.OPTION_BUFFS_RELATIVE_Y_TOOLTIP = "Ajustez verticalement la position initiale des buffs";
 l.OPTION_DEBUFFSSCALE = "Taille des d\195\169buffs "..required;
 l.OPTION_DEBUFFSSCALE_TOOLTIP = l.CY.."Par d\195\169faut dans Wow : 1"
 l.OPTION_MAXDEBUFFS = "Limite de d\195\169buffs"..required;
 l.OPTION_MAXDEBUFFS_TOOLTIP = "Nombre maximum de d\195\169buffs \195\160 afficher\n"..l.CY.."Par d\195\169faut dans Wow : "..ns.DEFAULT_MAXBUFFS;
 l.OPTION_MAXDEBUFFS_FORMAT = "%d |4d\195\169buff:d\195\169buffs";
 l.OPTION_DEBUFFSPERLINE = "D\195\169buffs par ligne"..required;
-l.OPTION_DEBUFFSPERLINE_TOOLTIP = "Nombre d'ic\195\180nes de d\195\169buff par ligne\n"..l.CY.."Par d\195\169faut dans Wow : max";
+l.OPTION_DEBUFFSPERLINE_TOOLTIP = "Nombre d'ic\195\180nes de d\195\169buff par ligne\n"..l.CY.."Ignor\195\169 si sup\195\169rieur \195\160 la Limite";
 l.OPTION_DEBUFFSPERLINE_FORMAT = "%d par ligne";
-l.OPTION_DEBUFFSVERTICAL = "Aligner les d\195\169buffs verticalement"..required;
-l.OPTION_DEBUFFSVERTICAL_TOOLTIP = "Les d\195\169buffs seront align\195\169s verticalement, en colonnes\n"..l.CY.."D\195\169sactiv\195\169 quelques secondes \195\160 la mort d'un boss";
+l.OPTION_DEBUFFSORIENTATION = "Orientation des d\195\169buffs"..required;
+l.OPTION_DEBUFFSORIENTATION_TOOLTIP = "Choisissez l\'arrangement des d\195\169buffs (supporte le multiligne)\n"..l.CY.."Par d\195\169faut : "..l.OPTION_ORIENTATION_RightThenUp;
+l.OPTION_DEBUFFS_RELATIVE_X = "D\195\169calage horizontal"..required;
+l.OPTION_DEBUFFS_RELATIVE_X_TOOLTIP = "Ajustez horizontalement la position initiale des d\195\169buffs";
+l.OPTION_DEBUFFS_RELATIVE_Y = "D\195\169calage vertical"..required;
+l.OPTION_DEBUFFS_RELATIVE_Y_TOOLTIP = "Ajustez verticalement la position initiale des d\195\169buffs";
 l.OPTION_USETAINTMETHOD = l.CY.."Affichage classique de la Limite de buffs / d\195\169buffs"..required.." "..l.ALERT
 l.OPTION_USETAINTMETHOD_TOOLTIP = "D\195\169coch\195\169, utilise l'affichage exp\195\169rimental\nCoch\195\169, utilise l'affichage stable, mais avec une "..l.RDL.."erreur par session|r, pas si grave..."
 l.OPTION_BUFFS_TAINTWARNING = l.ALERT.." Changer la Limite provoque une "..l.RDL.."erreur par session|r, pas si grave..."
 l.OPTION_BUFFS_FLICKERWARNING = l.INFO.." Le repositionnement peut \195\170tre affect\195\169 quelques secondes \195\160 la mort d'un boss"
+l.OPTION_BUFFS_RESET = "Annuler tout repositionnement"
 
 l.OPTION_RESET_OPTIONS = "R\195\169initialiser le profil";
 l.OPTION_RELOAD_REQUIRED = "Certains changements n\195\169cessitent un rechargement (\195\169crivez : "..l.YL.."/reload|r )";
