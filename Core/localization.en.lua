17feff1fa1d58a04b29fdab3d15813cb86129370
KaspOu <kaspbouh+gitkraken@gmail.com>
2025-05-07 04:35:13 +0200

    :hammer: Prepare localizations sync

--- Core/localization.en.lua
+++ Core/localization.en.lua
@@ -22,6 +22,7 @@ l.MSG_LOADED         = format("%s loaded and active", l.VERS_TITLE);
 l.INIT_FAILED = format("%s not initialized correctly (conflict?)!", l.VERS_TITLE);
 
 local required = l.YL.."*";
+-- KBD START
 l.OPTION_BUFFS_HEADER = "Buffs / Debuffs"
 l.OPTION_ORIENTATION_LeftThenUp = "Left, then Up"
 l.OPTION_ORIENTATION_LeftThenUp_Default = l.DEFAULT.."Left, then Up (default)"
@@ -39,10 +40,10 @@ l.OPTION_BUFFSPERLINE_TOOLTIP = "Number of buffs per line\n"..l.CY.."Ignored if 
 l.OPTION_BUFFSPERLINE_FORMAT = "%d per line"..required;
 l.OPTION_BUFFSORIENTATION = "Buffs orientation"..required;
 l.OPTION_BUFFSORIENTATION_TOOLTIP = "Choose how buffs are arranged (/w multiline support)\n"..l.CY.."Default: Left to Right, then Up"
-l.OPTION_BUFFS_RELATIVE_X = "Horizontal offset"..required;
-l.OPTION_BUFFS_RELATIVE_X_TOOLTIP = "Adjust the horizontal position of the buffs";
-l.OPTION_BUFFS_RELATIVE_Y = "Vertical offset"..required;
-l.OPTION_BUFFS_RELATIVE_Y_TOOLTIP = "Adjust the vertical position of the buffs";
+l.OPTION_BUFFS_RELATIVE_X = "Horizontal position"..required;
+l.OPTION_BUFFS_RELATIVE_X_TOOLTIP = "Adjust the relative horizontal position of the buffs";
+l.OPTION_BUFFS_RELATIVE_Y = "Vertical position"..required;
+l.OPTION_BUFFS_RELATIVE_Y_TOOLTIP = "Adjust the relative vertical position of the buffs";
 l.OPTION_DEBUFFSSCALE = "Debuffs relative size"..required;
 l.OPTION_DEBUFFSSCALE_TOOLTIP = l.CY.."Wow default: 1"
 l.OPTION_MAXDEBUFFS = "Max debuffs"..required;
@@ -53,15 +54,16 @@ l.OPTION_DEBUFFSPERLINE_TOOLTIP = "Number of debuff icons per line\n"..l.CY.."Ig
 l.OPTION_DEBUFFSPERLINE_FORMAT = "%d per line";
 l.OPTION_DEBUFFSORIENTATION = "Debuffs orientation"..required;
 l.OPTION_DEBUFFSORIENTATION_TOOLTIP = "Choose how debuffs are arranged ((/w multiline support)\n"..l.CY.."Default: Right to Left, then Up"
-l.OPTION_DEBUFFS_RELATIVE_X = "Horizontal offset"..required;
-l.OPTION_DEBUFFS_RELATIVE_X_TOOLTIP = "Adjust the horizontal position of the debuffs";
-l.OPTION_DEBUFFS_RELATIVE_Y = "Vertical offset"..required;
-l.OPTION_DEBUFFS_RELATIVE_Y_TOOLTIP = "Adjust the vertical position of the debuffs";
+l.OPTION_DEBUFFS_RELATIVE_X = "Horizontal position"..required;
+l.OPTION_DEBUFFS_RELATIVE_X_TOOLTIP = "Adjust the relative horizontal position of the debuffs";
+l.OPTION_DEBUFFS_RELATIVE_Y = "Vertical position"..required;
+l.OPTION_DEBUFFS_RELATIVE_Y_TOOLTIP = "Adjust the relative vertical position of the debuffs";
 l.OPTION_USETAINTMETHOD = l.CY.."Legacy display for Max buffs/debuffs"..required.." "..l.ALERT
 l.OPTION_USETAINTMETHOD_TOOLTIP = "Unchecked, uses an experimental display\nChecked, uses a stable display, but with one "..l.RDL.."error per session|r, not a big deal..."
 l.OPTION_BUFFS_TAINTWARNING = l.ALERT.." Changing Max buffs/debuffs causes one "..l.RDL.."error per session|r, not a big deal..."
 l.OPTION_BUFFS_FLICKERWARNING = l.INFO.." Repositioning may be affected for a few seconds after a boss kill"
 l.OPTION_BUFFS_RESET = "Cancel any repositioning"
+-- KBD END
 
 l.OPTION_RESET_OPTIONS = "Reset options";
 l.OPTION_RELOAD_REQUIRED = "Some changes require reloading (write: "..l.YL.."/reload|r )";
